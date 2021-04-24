28c88ed6f5b26c4c253815bd0cccdf966235b989
Editux <43064329+Editux@users.noreply.github.com>
2021-04-24 10:53:13 +0300

    Add files via upload

--- sprinboot_project/src/main/java/sample/ArticleServices.java
+++ sprinboot_project/src/main/java/sample/ArticleServices.java
@@ -2,25 +2,25 @@ package sample;
 import com.google.api.core.ApiFuture;
 import com.google.cloud.firestore.*;
 import com.google.firebase.cloud.FirestoreClient;
-import com.google.firebase.internal.NonNull;
-import javafx.concurrent.Task;
+
 import org.springframework.stereotype.Service;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
+
 
 @Service
 public class ArticleServices {
     public static final String COL_NAME="Articles";
 
-    public String saveArticleDetails(Articles articles) throws InterruptedException, ExecutionException {
+    public String saveArticleDetails(Articles articles) throws ExecutionException, InterruptedException {
         Firestore dbFirestore = FirestoreClient.getFirestore();
         ApiFuture<WriteResult> collectionsApiFuture = dbFirestore.collection(COL_NAME).document(articles.getTitle()).set(articles);
-        return collectionsApiFuture.get().getUpdateTime().toString(); }
 
-    public Articles getArticlesDetails(String title) throws InterruptedException, ExecutionException {
+            return collectionsApiFuture.get().getUpdateTime().toString(); }
+
+
+
+    public Articles getArticlesDetails(String title) throws ExecutionException, InterruptedException {
         Firestore dbFirestore = FirestoreClient.getFirestore();
         DocumentReference documentReference = dbFirestore.collection(COL_NAME).document(title);
         ApiFuture<DocumentSnapshot> future = documentReference.get();
